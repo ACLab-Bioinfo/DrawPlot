f421f1a9ab4762acac8b9f1a6b9c987c3a3ca234
patrickwong1104 <113508560+patrickwong1104@users.noreply.github.com>
2022-10-05 18:27:03 +0800

    Update Density_UMAPPlot_using_ggplot.R

--- Density_UMAPPlot_using_ggplot.R
+++ Density_UMAPPlot_using_ggplot.R
@@ -10,21 +10,6 @@ data <- cbind(Embeddings(object=caf_R[['umap']]), FetchData(caf_R, 'anno5'))
 # CAF non-responder
 data <- cbind(Embeddings(object=caf_NR[['umap']]), FetchData(caf_NR, 'anno5'))
 
-# CAF Pre-treatment
-data <- cbind(Embeddings(object=caf_Pre[['umap']]), FetchData(caf_Pre, 'anno5'))
-# CAF On-treatment
-data <- cbind(Embeddings(object=caf_On[['umap']]), FetchData(caf_On, 'anno5'))
-
-# CAF Edge
-data <- cbind(Embeddings(object=caf_Edge[['umap']]), FetchData(caf_Edge, 'anno5'))
-# CAF Core
-data <- cbind(Embeddings(object=caf_Core[['umap']]), FetchData(caf_Core, 'anno5'))
-
-# CAF responder & Pre/On
-data <- cbind(Embeddings(object=caf_NR_Pre_Core[['umap']]), FetchData(caf_NR_Pre_Core, 'anno5'))
-data <- cbind(Embeddings(object=caf_NR_On_Core[['umap']]), FetchData(caf_NR_On_Core, 'anno5'))
-
-
 p <- ggplot(data = data, mapping = aes(x = UMAP_1, y = UMAP_2)) + 
             geom_pointdensity() +
             scale_color_viridis() + theme_bw() +  
